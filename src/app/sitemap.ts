4b4e5d5e986e17601800c455ade5d547c0acb0fa
mathewschilongo099 <matthewchilongo553@gmail.com>
2026-06-08 13:43:06 +0200

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://vet-sphere.vercel.app',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vet-sphere.vercel.app/articles',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vet-sphere.vercel.app/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vet-sphere.vercel.app/contact',
+      lastModified: new Date(),
+    },
+  ];
+}
